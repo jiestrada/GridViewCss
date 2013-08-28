5b1dc4377b87d6eea47f9bb2789a19f2f508a9e2
José I. Estrada <jiestrada@live.com.mx>
2013-08-28 09:36:06 -0500

    Cómo dar estilo a un GridView con CSS
    Podemos utilizar las hojas de estilo en cascada para dar formato al
    control GridView. El siguiente ejemplo muestra cómo se puede formatear
    el control GridView utilizando hojas de estilo en cascada.

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="GridViewCss_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>GridView con estilo css</title>
+    <link href="Estilo.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" GridLines="None"
+                AllowPaging="true" CssClass="mGrid" PagerStyle-CssClass="pgr" AlternatingRowStyle-CssClass="alt"
+                PageSize="7" >
+                <Columns>
+                    <asp:BoundField DataField="EmpId" HeaderText="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Nombre" />
+                    <asp:BoundField DataField="Address" HeaderText="Dirección" />
+                    <asp:BoundField DataField="Date" HeaderText="Fecha" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data
+
+Partial Class GridViewCss_Default
+    Inherits System.Web.UI.Page
+
+    Private Sub BindData()
+        Dim dt As New DataTable()
+        dt.Columns.Add("EmpId", GetType(Integer))
+        dt.Columns.Add("Name", GetType(String))
+        dt.Columns.Add("Address", GetType(String))
+        dt.Columns.Add("Date", GetType(DateTime))
+        '
+        ' Here we add five DataRows.
+        '
+        dt.Rows.Add(25, "José", "México", DateTime.Now)
+        dt.Rows.Add(50, "Fransua Meme", "Francia", DateTime.Now)
+        dt.Rows.Add(10, "Ruben", "España", DateTime.Now)
+        dt.Rows.Add(21, "Aditya", "Italia", DateTime.Now)
+        dt.Rows.Add(100, "Jhon", "Inglaterra", DateTime.Now)
+
+        dt.Rows.Add(101, "ABC", "Delhi", DateTime.Now)
+        dt.Rows.Add(102, "XYZ", "Noida", DateTime.Now)
+        dt.Rows.Add(103, "BBC", "Delhi", DateTime.Now)
+        dt.Rows.Add(104, "CIA", "Banglore", DateTime.Now)
+        dt.Rows.Add(105, "KBC", "Delhi", DateTime.Now)
+        dt.Rows.Add(106, "Jack", "Noida", DateTime.Now)
+        dt.Rows.Add(107, "Jhon", "Gurgaon", DateTime.Now)
+        dt.Rows.Add(108, "Mali", "Gurgaon", DateTime.Now)
+        dt.Rows.Add(109, "Sara", "Noida", DateTime.Now)
+
+
+        GridView1.DataSource = dt
+        GridView1.DataBind()
+    End Sub
+
+    Protected Sub GridView1_PageIndexChanging(sender As Object, e As GridViewPageEventArgs) Handles GridView1.PageIndexChanging
+        GridView1.PageIndex = e.NewPageIndex
+        BindData()
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        BindData()
+    End Sub
+End Class
